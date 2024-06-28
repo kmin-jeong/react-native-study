05b9426cc809c2241307278a78b2dd021ef37cb8
kmin-jeong <desk535353@gmail.com>
2024-06-28 17:36:02 +0900

    fix: stackNavigator 추가 및 기능 수정

--- App.tsx
+++ App.tsx
@@ -6,29 +6,55 @@
  */
 
 import React from 'react';
-import {SafeAreaView, ScrollView, StyleSheet} from 'react-native';
+import {StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Navigator from './src/components/Navigator';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import Add from './src/components/Add';
+import Navigator from './src/components/Navigator';
+import {Back} from './src/components/button/Back';
+import {Complete} from './src/components/button/Complete';
+import {useThema, ThemeProvider} from './src/store/ThemeStore';
+import COLORS from './src/style/style';
 
 function App(): React.JSX.Element {
   const Stack = createNativeStackNavigator();
 
   return (
-    <GestureHandlerRootView style={{flex: 1}}>
-      <SafeAreaView style={styles.container}>
-        <NavigationContainer>
-          <Navigator />
-        </NavigationContainer>
-      </SafeAreaView>
-    </GestureHandlerRootView>
+    <ThemeProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen
+            name="Tab"
+            component={Navigator}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Add"
+            component={Add}
+            options={({navigation}) => {
+              const {themeColor} = useThema();
+              return {
+                headerShown: true,
+                headerShadowVisible: false,
+                headerTitle: '할 일을 추가해주세요!',
+                headerTitleStyle: {color: themeColor, fontSize: 18},
+                headerBackTitleVisible: false,
+                headerLeft: () => Back({navigation}),
+                headerRight: () => Complete({navigation}),
+                headerStyle: styles.header,
+              };
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    height: '100%',
+  header: {
+    backgroundColor: COLORS.gray,
   },
 });
 
